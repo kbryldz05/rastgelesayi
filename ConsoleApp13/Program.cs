1354d39d4506d65b203733979e39a762b0ed3a21
kbryldz05 <76847715+kbryldz05@users.noreply.github.com>
2021-03-08 14:07:26 +0300

    Add project files.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kaç sayı üretmek istiyorsunuz?");
+            int adet =int .Parse(Console.ReadLine());
+            
+            
+            Random rastgele = new Random();
+            int[] dizi = new int[adet];
+
+            for (int i = 0; i < 100; i++)
+            {
+                int yeniSayi = rastgele.Next(0, 1000);
+                Console.WriteLine(yeniSayi . ToString());
+
+                if (dizi.Contains(yeniSayi))
+                {
+                    i++;
+                } 
+                else
+                {
+                  dizi [i] = yeniSayi;
+                }
+
+
+            }Array.Sort(dizi);
+            for (int i = 0; i <dizi.Length; i++) ;
+            {
+
+                Console.Write("\n\nRasgele dizi\n\n" + dizi[0]);
+            }
+            Console.ReadKey();
+
+        }
+            
+       
+        }
+    }
